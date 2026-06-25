3b85688ea4a8daccde26cb05113557a4075a42bf
Muhammad Abrar <abrar.siddique5@gmail.com>
2026-06-25 14:03:03 +0500

    fix: network binding to 0.0.0.0 for railway proxy

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+ await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on port: ${port}`);
 }
 
 bootstrap();
